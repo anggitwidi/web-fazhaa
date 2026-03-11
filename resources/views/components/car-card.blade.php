c6e2a879df7c78f98c02561f38683a9aa6cb0c53
beben <bebensilaban@gmail.com>
2026-03-11 23:44:36 +0700

    110326

---
+++ resources/views/components/car-card.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-white rounded-[10px] shadow-md p-4">
+    <img src="{{ $image }}" class="mb-4">
+    <h3 class="font-semibold">{{ $name }}</h3>
+    <p class="text-gray-500">{{ $price }}</p>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,12 +13,11 @@
     <section class="py-16 container mx-auto">
         <h2 class="text-2xl font-bold mb-8">Lihat Yang Terbaru</h2>
 
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-            <div class="bg-white shadow rounded-xl p-4">
-                <img src="/images/car1.png" class="mb-4">
-                <h3 class="font-semibold">Raize</h3>
-            </div>
-        </div>
+        <x-car-card 
+            image="/images/raize.png"
+            name="Raize"
+            price="Rp 200.000.000"
+        />
     </section>
 
 @endsection
